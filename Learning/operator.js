f7ff26f2d08644ce324c0db311987113c46a9245
shahinBS23 <shahin.shanaous@brainstation.com>
2023-05-28 01:32:15 +0600

    Initialize

---
+++ Learning/operator.js
@@ -0,0 +1,11 @@
+// openerators
+let a = 10
+let b = 2
+console.log("a=" + a + " " + "b=" + b)
+ console.log("a + b: " + (a+b))
+ console.log("a - b: " + (a-b))
+ console.log("a * b: " + (a*b))
+ console.log("a / b: " + (a/b))
+ console.log("a % b: " + (a%b))
+ console.log("a ** b: " + (a**b))
+//  console.log("a b: " + (ab))
---
+++ Learning/variable.js
@@ -0,0 +1,16 @@
+// From here my new JS journy stared 
+var a = 20
+var b = 34
+console.log("Value of (a+b):" + (a+b))
+var a = 10
+let h =10
+
+{
+    var p = 10
+    var c = 2
+    console.log("Value of h:"+h)
+}
+
+console.log("Value of p: "+p)
+console.log("Value of c:"+c)
+
