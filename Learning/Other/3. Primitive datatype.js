45e27a5d6b87a27d9a457619ad81bc04f87431dd
shahinBS023 <shahin.brainstation23@gmail.com>
2023-06-02 18:32:21 +0600

    Premitive datatype

---
+++ Learning/Other/3. Primitive datatype.js
@@ -0,0 +1,11 @@
+// nn bb ss u
+
+let nul = null
+let number = 234
+let bool = false
+let bigNumber = BigInt(3) + BigInt(7382)
+let string = "Hello I am here..."
+let symbol = Symbol("This a symbol.")
+let und = undefined
+
+console.log(nul, number, bool, bigNumber, string, symbol, und)
