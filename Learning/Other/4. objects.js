24b0699c931720a41233e1e2a692356b2f195459
shahinBS023 <shahin.brainstation23@gmail.com>
2023-06-02 19:04:26 +0600

    Update objects

--- Learning/Other/4. objects.js
+++ Learning/Other/4. objects.js
@@ -1,7 +1,10 @@
 const myInfo = {
     "Name": "Shahin",
     "Date of birth": "12/12/1990",
-    "Address": "Chuadange"
+    "Address": {
+        "Permanent": "Chuadange",
+        "Presnt": "Dhaka"
+    }
 }
 
-console.log(myInfo["Date of birth"], myInfo.Address, myInfo["Name"])
+console.log(myInfo["Date of birth"], myInfo.Address.Presnt, myInfo["Name"], myInfo.Address["Permanent"])
